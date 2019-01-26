6f9711a3d31da4ed9d52a4896c0769898c59c767
Krzysztof Rytlewski <krzysztof.rytlewski@o2.pl>
2019-01-26 02:52:35 +0100

    finito

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 window.onload = function(){
+    var h = 12;
+    var m = 0;
+    
     setInterval(startTime, 1000);
-    setInterval(hour, 3000)
-    var h = 0;
-    var m = 10;
-    function hour(){  
-        h += 1;
-        while (h<10) {h = "0"+h};
-        return h;
-    };
+
     function startTime (){
-        m +=1;
-        document.getElementById("time").value = h+":"+m;
+        m += 1;
+        var mm;
+        var hh;
+        if (m<10) {mm = "0"+m} else {mm = m};
+        if (m>=59) {m = -1};
+        if (m<0) {h += 1};
+        if (h<10) {hh = "0"+h} else {hh = h};
+        if (h>=23) {h = -1};
+       
+        document.getElementById("time").value = hh+":"+mm;
     }
 }
