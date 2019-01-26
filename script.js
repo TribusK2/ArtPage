bcfeb5a8939277a7f0c4fa7ea9392862842f8d6d
Krzysztof Rytlewski <krzysztof.rytlewski@o2.pl>
2019-01-26 10:35:35 +0100

    clock function

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
 window.onload = function(){
-    var h = 12;
-    var m = 0;
-    
-    setInterval(startTime, 1000);
 
+    // Start counting at the <input> #time.
+    setInterval(startTime, 1000);
     function startTime (){
+        var h = Number(document.getElementById("time").value.slice(0,2));
+        var m = Number(document.getElementById("time").value.slice(3,5));
         m += 1;
-        var mm;
-        var hh;
-        if (m<10) {mm = "0"+m} else {mm = m};
-        if (m>=59) {m = -1};
-        if (m<0) {h += 1};
-        if (h<10) {hh = "0"+h} else {hh = h};
-        if (h>=23) {h = -1};
-       
+        
+        if (m < 10) {mm = "0"+m} else {mm = m};
+        if (m > 59) {mm = "0"+0 ; h += 1};
+        if (h < 10) {hh = "0"+h} else {hh = h};
+        if (h > 23) {hh = "0"+0};
         document.getElementById("time").value = hh+":"+mm;
     }
+
 }
